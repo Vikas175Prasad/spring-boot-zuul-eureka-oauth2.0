d9dfc23aaddb407c875ff1e5db359b1c6e8a9997
vikas <vikas@DESKTOP-LJSER9M>
2020-04-09 18:50:52 +0530

    first commit

---
+++ zuul-service/src/main/java/com/vikas/zuul/config/GatewayConfiguration.java
@@ -0,0 +1,22 @@
+package com.vikas.zuul.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//
+//@Configuration
+//@EnableResourceServer
+public class GatewayConfiguration  {
+//    @Override
+//    public void configure(final HttpSecurity http) throws Exception {
+//        http.authorizeRequests().
+//            antMatchers("/oauth/**").
+//            permitAll().
+//            antMatchers("/**").
+//            authenticated();
+//    }    
+//    
+//    @Override
+//    public void configure(final WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/**");
+//    }
+}
---
+++ zuul-service/src/test/java/com/vikas/zuul/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
